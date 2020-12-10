da86e467b9aa00317fcb01baa1abf14e6cc27aee
lamiasristy <lamiasristy@gmail.com>
2020-12-10 17:26:49 +0100

    Added HTML and CSS for the restaurant page.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,30 @@
 import _ from 'lodash';
 import './style.css';
+import Icon from './icon.jpg';
 
-function component() {
-    const element = document.createElement('div');
+import banner from './banner';
+
+banner();
+
+// function component() {
+//   const element = document.createElement('div');
+
+//   // Lodash, now imported by this script
+//   // element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+
+//   // Add the image to our existing div.
+//   // const myIcon = new Image();
+//   // myIcon.src = Icon;
+
+//   // element.appendChild(myIcon);
+
+//   element.innerHTML = banner();
   
-    // Lodash, now imported by this script
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+//   return element;
+//   }
+//   document.body.appendChild(component());
   
-    return element;
-  }
+
+
+
   
-  document.body.appendChild(component());
--- webpack.config.js
+++ webpack.config.js
@@ -15,6 +15,10 @@ module.exports = {
           'css-loader'
         ]
       },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
     ],
   },
 };
