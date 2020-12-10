e48b57c7247d69ffa1b5156c9c3a15aa07f8d331
lamiasristy <lamiasristy@gmail.com>
2020-12-10 12:31:22 +0100

    HTML skeleton added with single <div id:content>

--- webpack.config.js
+++ webpack.config.js
@@ -6,4 +6,15 @@ module.exports = {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+    ],
+  },
 };
